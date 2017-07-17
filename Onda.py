3745e59415e854bc15991ac787780554b884d9f4
santiagocaro <sebastian4131@gmail.com>
2017-07-17 11:58:49 -0500

    Segundas derivadas de x y y mioo

--- Onda.py
+++ Onda.py
@@ -7,12 +7,12 @@ grilla=np.zeros((puntos,puntos))
 grilla[(puntos/3),(puntos/2)]=-0.5
 dx2=grilla.copy()
 dy2=grilla.copy()
-print grilla
+#print grilla
 delta=L/puntos
 #print delta
 for i in range(1,puntos-1):
 	for j in range(1,puntos-1):
 		dx2[i,j]=(grilla[i,j+1]-2*grilla[i,j]+grilla[i,j-1])/delta**2
 		dy2[i,j]=(grilla[i+1,j]-2*grilla[i,j]+grilla[i-1,j])/delta**2
-print dx2
+#print dx2
 
