56761bcf0cb08c86b735810f5f567826fd73b836
Sebastiandt41 <sebastian4131@gmail.com>
2017-07-17 11:44:03 -0500

    Segundas derivadas de x y y

---
+++ Onda.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+L=30.0
+puntos=10
+grilla=np.zeros((puntos,puntos))
+grilla[(puntos/3),(puntos/2)]=-0.5
+dx2=grilla.copy()
+dy2=grilla.copy()
+print grilla
+delta=L/puntos
+#print delta
+for i in range(1,puntos-1):
+	for j in range(1,puntos-1):
+		dx2[i,j]=(grilla[i,j+1]-2*grilla[i,j]+grilla[i,j-1])/delta**2
+		dy2[i,j]=(grilla[i+1,j]-2*grilla[i,j]+grilla[i-1,j])/delta**2
+print dx2
+
